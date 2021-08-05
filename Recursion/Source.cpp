77ec3a4d35552806d605cdea37258182c77cfd0f
yandutka@mail.ru <yandutka@mail.ru>
2021-08-05 21:41:32 +0300

    Add project files.

---
+++ Recursion/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void elevator(int floor)
+{
+	if (floor == 0) //Условие выхода из рекурсии
+	{
+		cout << "Вы в подвале" << endl;
+		return;
+	}
+	cout << floor << endl;
+	elevator(floor - 1);   //<- Рекурсия
+	cout << floor << endl; //<- Рекурсивный возврат
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	/*cout << "Hello World";
+	main()*/
+	
+	int floor;
+	cout << "Введите номер этажа: "; cin >> floor;
+	elevator(floor);
+}
